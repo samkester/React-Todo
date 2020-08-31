4fe1c9757634d02c501f49b6e19d57fbcee68dc6
Sam Kester <samkester@gmail.com>
2020-08-31 15:39:38 -0600

    styled App

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,5 +1,16 @@
 const theme = {
-
+    color: {
+        page: "lightslategray",
+        UI: "slategray",
+        post: "skyblue",
+        completed: "silver",
+    },
+    background: {
+        page: "dimgray",
+        UI: "aliceblue",
+        post: "#00002f",
+        completed: "#2f2f2f"
+    },
 }
 
 export default theme;
