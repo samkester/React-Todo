a0afba26868a77d5dcaf3d27aea6b4f682a50811
Sam Kester <samkester@gmail.com>
2020-08-31 14:13:33 -0600

    Form state + validation(-ish)

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,17 +6,69 @@ class TodoForm extends React.Component {
     constructor(){
         super();
         this.state = {
-            fields: {},
+            fields: {text: ""},
             errors: {},
             enabled: false,
         };
     }
 
+    updateField = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        console.log(value);
+
+        this.setState({
+            fields: {
+                ...this.state.fields,
+                [name]: value,
+            }
+        });
+
+        // put validation checks here
+        if(value.trim() === ""){
+            this.setState({
+                errors: {
+                    ...this.state.errors,
+                    [name]: "Please enter a todo item.",
+                }
+            })
+        }
+        else{
+            this.setState({
+                errors: {
+                    ...this.state.errors,
+                    [name]: "",
+                }
+            })
+        }
+        
+        this.updateButtonState();
+    };
+
+    updateButtonState = () => {
+        this.setState({
+            enabled: this.state.name && this.state.name !== "",
+        });
+    }
+
+    createNewTodo = event => {
+        event.preventDefault();
+    };
+
     render () {
         return (
-            <form>
-                I am a todo form.
-            </form>
+            <div>
+                <form onSubmit={this.createNewTodo}>
+                    <input type="text" name="text" value={this.state.fields.text} onChange={this.updateField} />
+                    <button onClick={this.createNewTodo}>Push Me</button>
+                </form>
+                <div className="errors">
+                    {this.state.errors &&
+                    Object.values(this.state.errors).map((error, index) => <p key={index}>{error}</p>) /* for each field in "errors", create a <p> with that field's value */
+                    }
+                </div>
+            </div>
         )
     }
 }
