b00121367ed0ab42158ccd9ad332668568b12a08
Sam Kester <samkester@gmail.com>
2020-08-31 13:45:34 -0600

    state + props

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 
 // App [state: list of todo items; search filter]
-//   SearchBar [props: set search filter]
-//   TodoList [props: list of todo items; removeItem]
-//     Todo [props: specific todo item; removeItem]
+//   SearchBar [props: search filter, set search filter, remove cleared items]
+//   TodoList [props: list of todo items, remove item, toggle item]
+//     Todo [props: specific todo item, remove item, toggle item]
 //   TodoForm [state: form fields, errors, and button enabling / props: addItem ]
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todoItems: [],
+      searchTerm: "",
+    };
+  }
+
+  setSearchTerm = () => {};
+
+  addItem = () => {};
+  toggleItem = () => {};
+  removeItem = () => {};
+  removeAllDisabledItems = () => {};
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <SearchBar />
-        <TodoList />
-        <TodoForm />        
+        <SearchBar searchTerm={this.state.searchTerm} setSearchTerm={this.setSearchTerm} removeAllCleared={this.removeAllDisabledItems} />
+        <TodoList listItems={this.state.todoItems} removeItem={this.removeItem} toggleItem={this.toggleItem} />
+        <TodoForm addItem={this.addItem} />
       </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,15 @@ import React from "react";
 //   TodoForm [state: form fields, errors, and button enabling / props: addItem ]
 
 class TodoForm extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            fields: {},
+            errors: {},
+            enabled: false,
+        };
+    }
+
     render () {
         return (
             <form>
